cb664aa30dfc40abb2b780884600ddd196ca2ffe
Sergio Guerrero <sguerrero1971@gmail.com>
2016-03-19 20:21:25 -0400

    Assignment 31 CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index {|post, index| post.title = "Spam" if index % 5 == 0}
+
   end
 
   def show
