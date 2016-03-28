969d4041ebbc2f0e9cbc4ffb85221929447f1e92
Sergio Guerrero <sguerrero1971@gmail.com>
2016-03-28 16:51:21 -0400

    assignment-36-authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_save {self.name = name.split(/\s/).capitalize!.join}
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  validates_uniqueness_of :name, case_sensitive: false
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
 
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
+  it { is_expected.to validate_uniqueness_of(:name).case_sensitive}
 
 
   it { is_expected.to validate_presence_of(:email) }
