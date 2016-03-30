62c40297e210b3e6ef88af48d99fb116cebeee1e
Sergio Guerrero <sguerrero1971@gmail.com>
2016-03-30 11:50:38 -0400

    Checkpoint 39 Posts and Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'random_data'
 
+5.times do
+   User.create!(
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+ )
+ end
+ users = User.all
+
 15.times do
    Topic.create!(
      name:         RandomData.random_sentence,
@@ -18,6 +27,7 @@ require 'random_data'
  50.times do
 
    Post.create!(
+     user:   users.sample,
      topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -33,7 +43,14 @@ require 'random_data'
    )
  end
 
+ user = User.first
+ user.update_attributes!(
+  email: 'sguerrero1971@gmail.com',
+  password: 'helloworld'
+  )
+
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
--- lib/random_data.rb
+++ lib/random_data.rb
@@ -17,10 +17,20 @@ module RandomData
      sentence = strings.join(" ")
      sentence.capitalize << "."
    end
- 
+
    def self.random_word
      letters = ('a'..'z').to_a
      letters.shuffle!
      letters[0,rand(3..8)].join
+   end
+
+   def self.random_name
+      first_name = random_word.capitalize
+      last_name = random_word.capitalize
+      "#{first_name} #{last_name}"
+   end
+
+   def self.random_email
+     "#{random_word}@#{random_word}.#{random_word}"
    end
  end
