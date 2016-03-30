32b5f511e6b4b838b3c2a97eabaa3600f1c5a953
Sergio Guerrero <sguerrero1971@gmail.com>
2016-03-29 20:09:01 -0400

    Checkpoint 38 signin

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
