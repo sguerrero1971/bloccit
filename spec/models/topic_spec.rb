604673e7c89f17cd850eec03571aa308dd724d34
Sergio Guerrero <sguerrero1971@gmail.com>
2016-03-31 13:45:44 -0400

    checkpoint 42 labels

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe Topic, type: :model do
 
      it { is_expected.to have_many(:posts) }
 
+     it { is_expected.to have_many(:labelings) }
+
+     it { is_expected.to have_many(:labels).through(:labelings) }
+
+
      describe "attributes" do
        it "responds to name" do
          expect(topic).to respond_to(:name)
