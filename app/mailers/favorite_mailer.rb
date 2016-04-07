ba3a2d990f391e03d08e4ba82415acc2707eb33a
Sergio Guerrero <sguerrero1971@gmail.com>
2016-04-07 13:30:03 -0400

    assignment-44-favorites

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,15 +2,25 @@ class FavoriteMailer < ApplicationMailer
   default from: "sguerrero1971@gmail.com"
 
   def new_comment(user, post, comment)
+    headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
 
-  headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-  headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-  headers["References"] = "<post/#{post.id}@your-app-name.example>"
+    @user = user
+    @post = post
+    @comment = comment
 
-  @user = user
-  @post = post
-  @comment = comment
+    mail(to: user.email, subject: "New comment on #{post.title}")
+  end
 
-  mail(to: user.email, subject: "New comment on #{post.title}")
-end
+  def new_post(user, post)
+    headers["Message-ID"] = "<post/#{post.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @user = user
+    @post = post
+
+    mail(to: user.email, subject: "New post on #{post.title}")
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -32,4 +32,14 @@ class Post < ActiveRecord::Base
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
   end
+
+  after_create :send_post_emails
+
+  private
+
+    def send_post_emails
+      post.each do |post|
+        FavoriteMailer.new_post(post.user, post).deliver_now
+      end
+    end
 end
