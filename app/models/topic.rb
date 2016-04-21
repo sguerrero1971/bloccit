b36397514a7e2bd80908a6592e4c4e6ebe82b7dd
Sergio Guerrero <sguerrero1971@gmail.com>
2016-04-20 20:33:28 -0400

    Assignment 46 Private Topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,9 +1,11 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
-  
+
   has_many :labelings, as: :labelable
 
   has_many :labels, through: :labelings
 
+  scope :publically_viewable, -> { where(public: true)}
+  scope :privately_viewable, -> { where(public: false)}
   scope :visible_to, -> (user) { user ? all : where(public: true) }
 end
