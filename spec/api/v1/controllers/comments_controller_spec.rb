ed8812d764013923d5104771a79db9f813b83838
Sergio Guerrero <sguerrero1971@gmail.com>
2016-05-03 12:23:49 -0400

    Assignment 48 API retrieve

--- spec/api/v1/controllers/comments_controller_spec.rb
+++ spec/api/v1/controllers/comments_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
- RSpec.describe Api::V1::TopicsController, type: :controller do
+ RSpec.describe Api::V1::CommentsController, type: :controller do
    let(:my_user) { create(:user) }
    let(:my_topic) { create(:topic) }
    let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+   let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, user: my_user) }
 
    context "unauthenticated user" do
      it "GET index returns http success" do
@@ -12,14 +13,15 @@ require 'rails_helper'
      end
 
      it "GET show returns http success" do
-       get :show, id: my_topic.id
+       get :show, id: my_comment.id
        expect(response).to have_http_status(:success)
      end
 
-     it "GET show returns post" do
-       get :show, id: my_topic.id
+     it "GET show returns comment" do
+       get :show, id: my_comment.id
        comment_body = JSON.parse response.body
-       expect(comment_body["posts"]).to_not be_nil
+       expect(comment_body["id"]).to eq  my_comment.id
+       expect(comment_body["body"]).to eq my_comment.body
      end
    end
 
@@ -34,14 +36,15 @@ require 'rails_helper'
      end
 
      it "GET show returns http success" do
-       get :show, id: my_topic.id
+       get :show, id: my_comment.id
        expect(response).to have_http_status(:success)
      end
 
-     it "GET show returns post" do
-       get :show, id: my_topic.id
+     it "GET show returns comment" do
+       get :show, id: my_comment.id
        comment_body = JSON.parse response.body
-       expect(comment_body["posts"]).to_not be_nil
+       expect(comment_body["id"]).to eq  my_comment.id
+       expect(comment_body["body"]).to eq my_comment.body
      end
    end
  end
--- spec/api/v1/controllers/posts_controller_spec.rb
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
- RSpec.describe Api::V1::TopicsController, type: :controller do
+ RSpec.describe Api::V1::PostsController, type: :controller do
    let(:my_user) { create(:user) }
    let(:my_topic) { create(:topic) }
    let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+   let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, user: my_user) }
 
    context "unauthenticated user" do
      it "GET index returns http success" do
@@ -12,14 +13,14 @@ require 'rails_helper'
      end
 
      it "GET show returns http success" do
-       get :show, id: my_topic.id
+       get :show, id: my_post.id
        expect(response).to have_http_status(:success)
      end
 
-     it "GET show returns post" do
-       get :show, id: my_topic.id
+     it "GET show returns post comments" do
+       get :show, id: my_post.id
        comment_body = JSON.parse response.body
-       expect(comment_body["posts"]).to_not be_nil
+       expect(comment_body["comments"]).to_not be_nil
      end
    end
 
@@ -34,14 +35,14 @@ require 'rails_helper'
      end
 
      it "GET show returns http success" do
-       get :show, id: my_topic.id
+       get :show, id: my_post.id
        expect(response).to have_http_status(:success)
      end
 
-     it "GET show returns post" do
-       get :show, id: my_topic.id
+     it "GET show returns post comments" do
+       get :show, id: my_post.id
        comment_body = JSON.parse response.body
-       expect(comment_body["posts"]).to_not be_nil
+       expect(comment_body["comments"]).to_not be_nil
      end
    end
  end
